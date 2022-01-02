ec72afcf7e21b8fbaa25ca29678ed029fc8f91a0
OrzCat <f8h5k3@gmail.com>
2022-01-03 06:10:44 +0800

    new year change new skin

---
+++ public/javascripts/custom.js
@@ -0,0 +1,123 @@
+$(function() {
+    
+    "use strict";
+    
+    var wind = $(window);
+    
+    // ScrollIt
+    $.scrollIt({
+      upKey: 38,                // key code to navigate to the next section
+      downKey: 40,              // key code to navigate to the previous section
+      easing: 'swing',          // the easing function for animation
+      scrollTime: 600,          // how long (in ms) the animation takes
+      activeClass: 'active',    // class given to the active nav element
+      onPageChange: null,       // function(pageIndex) that is called when page is changed
+      topOffset: -70            // offste (in px) for fixed top navigation
+    });
+    
+    // Navbar scrolling background
+    wind.on("scroll",function () {
+        var bodyScroll = wind.scrollTop(),
+            navbar = $(".navbar-head"),
+            logo = $(".navbar-head .logo> img");
+        if(bodyScroll > 100){
+            navbar.addClass("nav-scroll");
+            logo.attr('src', 'img/logo.png');
+        }else{
+            navbar.removeClass("nav-scroll");
+            logo.attr('src', 'img/logo.png');
+        }
+    });
+    
+    // Close navbar-collapse when a  clicked
+    $(".navbar-nav .dropdown-item a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+    
+    // Sections background image from data background
+    var pageSection = $(".bg-img, section");
+    pageSection.each(function(indx){
+        if ($(this).attr("data-background")){
+            $(this).css("background-image", "url(" + $(this).data("background") + ")");
+        }
+    });
+
+    // Animations
+    var contentWayPoint = function () {
+        var i = 0;
+        $('.animate-box').waypoint(function (direction) {
+            if (direction === 'down' && !$(this.element).hasClass('animated')) {
+                i++;
+                $(this.element).addClass('item-animate');
+                setTimeout(function () {
+                    $('body .animate-box.item-animate').each(function (k) {
+                        var el = $(this);
+                        setTimeout(function () {
+                            var effect = el.data('animate-effect');
+                            if (effect === 'fadeIn') {
+                                el.addClass('fadeIn animated');
+                            }
+                            else if (effect === 'fadeInLeft') {
+                                el.addClass('fadeInLeft animated');
+                            }
+                            else if (effect === 'fadeInRight') {
+                                el.addClass('fadeInRight animated');
+                            }
+                            else {
+                                el.addClass('fadeInUp animated');
+                            }
+                            el.removeClass('item-animate');
+                        }, k * 200, 'easeInOutExpo');
+                    });
+                }, 100);
+            }
+        }, {
+            offset: '85%'
+        });
+    };
+    $(function () {
+        contentWayPoint();
+    });
+    
+    //  Scroll back to top
+    var progressPath = document.querySelector('.progress-wrap path');
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+    var updateProgress = function () {
+        var scroll = $(window).scrollTop();
+        var height = $(document).height() - $(window).height();
+        var progress = pathLength - (scroll * pathLength / height);
+        progressPath.style.strokeDashoffset = progress;
+    }
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 150;
+    var duration = 550;
+    jQuery(window).on('scroll', function () {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.progress-wrap').addClass('active-progress');
+        } else {
+            jQuery('.progress-wrap').removeClass('active-progress');
+        }
+    });
+    jQuery('.progress-wrap').on('click', function (event) {
+        event.preventDefault();
+        jQuery('html, body').animate({ scrollTop: 0 }, duration);
+        return false;
+    })
+    
+});
+
+// Preloader
+paceOptions = {
+    ajax: true,
+    document: true,
+    eventLag: false
+};
+Pace.on('done', function() {
+  $('#preloader').delay(500).fadeOut(800);
+});
